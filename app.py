2c370fa6a9fa6cbbfe0b464f926d782a782eab07
Athitaya6 <athitayakakandee@gmail.com>
2023-03-28 11:10:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 st.image("./pic/images.jpg")
+
+
 html_8 = """
 <div style="background-color:#99DF99;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>การทำนายข้อมูลดอกไม้</h5></center>
@@ -15,4 +17,6 @@ html_8 = """
 
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
-print('Athitaya')
+st.header("การจำแนกข้อมูล")
+st.subheader("1.ตัวอย่างการจำแนกข้อมูล")
+st.balloons()
